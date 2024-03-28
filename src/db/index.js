1f4ed4048e3e298ed6bdceb8e2def9c3402a3188
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-03-28 21:31:35 +0500

    SSL xatoligi to'g'irlandi

--- src/db/index.js
+++ src/db/index.js
@@ -2,5 +2,5 @@
 const pgp = require('../conf/pgp')
 module.exports = pgp(
     (process.env.NODE_ENV === 'production') ?
-        'postgres://bazar:kSpZrdAu8W9YBDKyAMGQh75U48L2qwHK@dpg-cnsrmlv79t8c73a85mcg-a/bazar_y558?ssl=true' :
+        'postgres://bazar:kSpZrdAu8W9YBDKyAMGQh75U48L2qwHK@dpg-cnsrmlv79t8c73a85mcg-a/bazar_y558' :
         'postgres://bazar:kSpZrdAu8W9YBDKyAMGQh75U48L2qwHK@dpg-cnsrmlv79t8c73a85mcg-a.frankfurt-postgres.render.com/bazar_y558?ssl=true')
