ad9ff1c0725da6f1dbf1d6ebcf33afcf345afa3d
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-04-04 21:10:54 +0500

    Bazaga ulanish o'zgartirildi Ahostga.

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,11 @@
 'use strict'
 const pgp = require('../conf/pgp')
-module.exports = pgp(
-    (process.env.NODE_ENV === 'production') ?
-        'postgres://bazar:kSpZrdAu8W9YBDKyAMGQh75U48L2qwHK@dpg-cnsrmlv79t8c73a85mcg-a/bazar_y558' :
-        'postgres://bazar:kSpZrdAu8W9YBDKyAMGQh75U48L2qwHK@dpg-cnsrmlv79t8c73a85mcg-a.frankfurt-postgres.render.com/bazar_y558?ssl=true')
+
+module.exports = pgp({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ?? 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER_NAME,
+    password: process.env.DB_USER_PASSWORD,
+    max: 30
+})
