874a10bc1b342c6aa92560a98cc453be08978f98
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-04-04 21:13:11 +0500

    Kichik o'zgarish

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,7 @@
 const pgp = require('../conf/pgp')
 
 module.exports = pgp({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ?? 5432,
+    host: 'localhost',
     database: process.env.DB_NAME,
     user: process.env.DB_USER_NAME,
     password: process.env.DB_USER_PASSWORD,
