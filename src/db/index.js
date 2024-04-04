0f153851fb8a5028d036826283dc79b5e9ebab38
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-04-04 21:19:32 +0500

    SSL yoqildi bazaga ulanishda.

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,11 @@
 const pgp = require('../conf/pgp')
 
 module.exports = pgp({
-    host: 'localhost',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ?? 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER_NAME,
     password: process.env.DB_USER_PASSWORD,
-    max: 30
+    max: 30,
+    ssl: true
 })
