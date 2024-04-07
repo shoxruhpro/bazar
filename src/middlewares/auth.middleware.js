e08f0e5fc18e771e0d0449456609853cc950dfc4
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-04-07 11:42:09 +0500

    Error handling qo'shidldi express-jwt uchun.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,2 +1,10 @@
 const { expressjwt } = require('express-jwt')
-module.exports = expressjwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] })
+module.exports = [
+    expressjwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }),
+    (err, req, res, next) => {
+        if (err)
+            res.status(err.status).json(err)
+        else
+            next()
+    }
+]
