97f4d0bd21961a914a28fc9a86a6c9849c05e9c9
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-04-12 09:53:10 +0500

    Last login kiritilidi.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,16 @@
 const { expressjwt } = require('express-jwt')
+const db = require('../db')
+
+
 module.exports = [
     expressjwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }),
-    (err, req, res, next) => {
+    async (err, req, res, next) => {
         if (err)
             res.status(err.status).json(err)
-        else
+        else {
+            const result = await db.result('UPDATE users SET last_login = $1 WHERE user_id = $2', [Date.now() / 1000, req.auth.user_id])
+            console.log(result)
             next()
+        }
     }
 ]
