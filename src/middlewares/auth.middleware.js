8617ab613cb1db5d65a81f7d84375a0ee7371887
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-04-12 10:04:46 +0500

    Last visit to'g'irlandi.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,10 +5,11 @@ const db = require('../db')
 module.exports = [
     expressjwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }),
     async (err, req, res, next) => {
-        if (err)
+        if (err) {
             res.status(err.status).json(err)
-        else {
-            const result = await db.result('UPDATE users SET last_visit = $1 WHERE user_id = $2', [Date.now() / 1000, req.auth.user_id])
+        } else {
+            const result = await db.result('UPDATE users SET last_visit = $1 WHERE user_id = $2',
+                [new Date().toISOString(), req.auth.user_id])
             console.log(result)
             next()
         }
