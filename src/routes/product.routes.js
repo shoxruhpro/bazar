b6d0550d5c24d62a637816043ff402f9272dc1a2
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2024-04-14 10:08:11 +0500

    tariff_id to'g'irlandi.

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -89,7 +89,7 @@ router.route('/')
                 'FROM products AS p INNER JOIN categories c ON p.category_id = c.category_id ' +
                 'INNER JOIN tops ON p.product_id = tops.product_id ' +
                 'INNER JOIN tariffs t ON tops.tariff_id = t.tariff_id AND tops.verified_at IS NOT NULL ' +
-                filter + ' ORDER BY tariff_id DESC', req.query)
+                filter + ' ORDER BY t.tariff_id DESC', req.query)
 
             res.json(products)
         } catch (e) {
